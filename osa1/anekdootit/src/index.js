c4940eaef817630db6f9870c60be5e07149ab31e
Samuli Löf <samuli.lof@gmail.com>
2019-03-23 21:00:05 +0200

    Anekdootit tehty

--- osa1/anekdootit/src/index.js
+++ osa1/anekdootit/src/index.js
@@ -1,63 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Header = props => {
-    return (
-        <h1>{props.course}</h1>
-    )
-}
-const Content = props => {
-    return (
-        <div>
-            {props.parts.map(p => (
-                <Part name={p.name} exercises={p.exercises} />
-            ))}
-        </div>
-    )
-}
-const Part = props => {
-    return (
-        <p>
-            {props.name} {props.exercises}
-        </p>
-    )
-}
-const Total = props => {
-    let total = 0;
-    props.parts.forEach(e => {
-        total += e.exercises;
-    });
-    return (
-        <p>yhteensä {total} tehtävää</p>
-    )
-}
+const App = (props) => {
+    const [selected, setSelected] = useState(0)
+    const [votes, setVotes] = useState(new Array(anecdotes.length + 1).join('0').split('').map(parseFloat))
 
-const App = () => {
-    const course = {
-        name: 'Half Stack -sovelluskehitys',
-        parts: [
-            {
-                name: 'Reactin perusteet',
-                exercises: 10
-            },
-            {
-                name: 'Tiedonvälitys propseilla',
-                exercises: 7
-            },
-            {
-                name: 'Komponenttien tila',
-                exercises: 14
-            }
-        ]
+    const addVote = () => {
+        const copy = [...votes]
+        copy[selected] += 1
+        setVotes(copy)
     }
 
+    let index = 0
+    votes.forEach((v, i) => {
+        if (v > votes[index]) index = i;
+    })
     return (
         <div>
-            <Header course={course.name} />
-            <Content parts={course.parts} />
-            <Total parts={course.parts} />
-        </div>
+            <h2>Anecdote of the day</h2>
+            <p>{props.anecdotes[selected]}</p>
+            <p>has {votes[selected]} votes</p>
+            <button onClick={addVote}>vote</button>
+            <button onClick={() => setSelected(getRand())}>next anecdote</button>
+
+            <h2>Anecdote with most votes</h2>
+            <p>{props.anecdotes[index]}</p>
+            <p>has {votes[index]} votes</p>
+        </div >
     )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+function getRand() {
+    return Math.floor(Math.random() * anecdotes.length);
+}
+
+const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+ReactDOM.render(
+    <App anecdotes={anecdotes} />,
+    document.getElementById('root')
+)
